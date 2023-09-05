831c0b40d7577c319608c2f7388d821e63f6123c
kashan <s@gmail.com>
2023-09-06 03:57:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+
+const encrypt=require('mongoose-encryption');
+
+const app =express();
+
+app.use(express.static('public'));
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://0.0.0.0:27017/userDB');
+
+const userSchema=new mongoose.Schema ({
+    email:String ,
+    password:String
+});
+
+const secret="thisismyschemasecurity.";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+
+const User=mongoose.model('User',userSchema);
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.render('home');
+});
+app.get("/login",(req,res)=>{
+    res.render('login');
+});
+app.get("/register",(req,res)=>{
+    res.render('register');
+});
+
+app.post("/register",(req,res)=>{
+
+    
+    const user=new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    user.save().then(()=>res.render('secrets'))
+    .catch((err)=>console.log(err));
+
+});
+
+app.post('/login',(req,res)=>{
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username})
+    .then((found)=>{
+        if(found.password === password){
+            res.render('secrets');
+        }
+    
+    })
+    .catch((err)=>console.log(err));
+
+});
+
+
+
+app.listen(3000,()=>{
+    console.log('started');
+})
